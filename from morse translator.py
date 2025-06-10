e6adb9920c75e2ea9ea8ad2a388f1755df8f8d20
Isaac Gibbons <isaac7922@outlook.com>
2025-06-09 19:20:45 -0700

    Update from morse translator.py

--- from morse translator.py
+++ from morse translator.py
@@ -1,17 +1,31 @@
-morse = input("Input the morse code to be translated, with spaces between the different sequences and a space at the end: ")
-morse_list = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."," "]
-english_list = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," "]
-code = ""
-translation = ""
-letters = []
-for character in morse:
-    if character != " ":
-        code += character
-    elif character == " ":
-        letters.append(code)
-        code = ""
-for sequence in letters:
-    equivalent = morse_list.index(sequence)
-    english = english_list[equivalent]
-    translation += english
-print(translation)
+def from_morse(morse)
+    morse_list = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."," "]
+    english_list = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," "]
+    code = ""
+    translation = ""
+    letters = []
+    for character in morse:
+        if character != " ":
+            code += character
+        elif character == " ":
+            letters.append(code)
+            code = ""
+    for sequence in letters:
+        equivalent = morse_list.index(sequence)
+        english = english_list[equivalent]
+        translation += english
+    return translation
+
+loop = True
+while loop:
+    morse_input = input("Input the morse code to be translated, with spaces between the different sequences and a space at the end: ")
+    english_output = from_morse(morse_input)
+    print("English translation:")
+    print(english_output)
+
+    print("Would you like to restart? (yes/no)")
+    loop_response = input().lower()
+    if loop_response == "yes":
+        continue
+    else:
+        loop = False
